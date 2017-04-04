0b3841ebdeced43a91ef6e0a51528ae4f429aa02
Jamie Nola <jnola@vibrenthealth.com>
2017-04-04 15:02:22 -0400

    Adds color page and updates color variables in SCSS

---
+++ app/scripts/main/color.controller.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jamienola on 4/4/17.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('vbr-style-guide')
+        .controller('ExampleController',
+
+        function ColorController() {
+            var col = this;
+
+
+        });
+
+})();
+
