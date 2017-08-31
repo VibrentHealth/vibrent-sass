8a2d2197c029886dbb070ee86b19461c3d7de630
Jamie Nola <jnola@vibrenthealth.com>
2017-08-31 10:39:47 -0400

    Adds check for null and undefined

--- app/scripts/main/stringMatch.directive.js
+++ app/scripts/main/stringMatch.directive.js
@@ -11,12 +11,12 @@
                 link: function (scope, element, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
                         var origin = scope.$eval(attrs["string-match"]);
-                        if (origin !== viewValue) {
-                            ctrl.$setValidity("match", false);
-                            return undefined;
-                        } else {
+                        if (origin === null || origin === undefined || origin === viewValue) {
                             ctrl.$setValidity("match", true);
                             return viewValue;
+                        } else {
+                            ctrl.$setValidity("match", false);
+                            return undefined;
                         }
                     });
 
