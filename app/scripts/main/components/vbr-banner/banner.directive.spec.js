78775fc78eea3eb285355c39c9fcd08341f62449
millsky <kmills@vibrenthealth.com>
2017-09-18 11:05:46 -0400

    Additional unit tests for vibrent-sass components

--- app/scripts/main/components/vbr-banner/banner.directive.spec.js
+++ app/scripts/main/components/vbr-banner/banner.directive.spec.js
@@ -10,6 +10,8 @@ var expect = chai.expect;
 describe('Banner Directive', function () {
     var $compile,
         $rootScope,
+        BannerService,
+        $timeout,
         element,
         scope,
         $templateCache;
@@ -25,10 +27,12 @@ describe('Banner Directive', function () {
 
     // Store references to $rootScope and $compile
     // so they are available to all tests in this describe block
-    beforeEach(inject(function(_$compile_, _$rootScope_,_$templateCache_){
+    beforeEach(inject(function(_$compile_, _$rootScope_,_$templateCache_,_BannerService_,_$timeout_){
         // The injector unwraps the underscores (_) from around the parameter names when matching
         $compile = _$compile_;
         $rootScope = _$rootScope_;
+        $timeout = _$timeout_;
+        BannerService = _BannerService_;
         scope = $rootScope.$new();
         $templateCache = _$templateCache_;
     }));
@@ -40,12 +44,55 @@ describe('Banner Directive', function () {
         expect(true).to.equal(true);
     });
 
-    it('The class should init with success',function () {
+    it('Should init with a success class',function () {
         element = $compile("<vbr-banner name='test-banner'></vbr-banner>")(scope);
         scope.$digest();
         var div = element.find('div')[0];
         expect(div.classList.contains('success')).to.equal(true);
     });
 
+    it('Should update the name when the scope is updated' ,function () {
+        element = $compile("<vbr-banner name='other-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        var isolatedScope = element.isolateScope().vm;
+        expect(isolatedScope.name).to.equal('other-banner');
+    });
+
+    it('Should update the scope when the bannerService is called',function () {
+        element = $compile("<vbr-banner name='other-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        BannerService.set('other-banner',{visible:true});
+        scope.$digest();
+        var isolatedScope = element.isolateScope().vm;
+        expect(isolatedScope.visible).to.equal(true);
+    });
+
+    it('Should Become Hidden after two seconds',function () {
+        element = $compile("<vbr-banner name='other-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        BannerService.set('other-banner',{visible:true,visibilityDuration:100});
+        scope.$digest();
+        var isolatedScope = element.isolateScope().vm;
+        $timeout.flush();
+        expect(isolatedScope.visible).to.equal(false);
+    });
+
+    it('Should not allow the user to close the modal when canClose is set to false',function () {
+        element = $compile("<vbr-banner name='other-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        BannerService.set('other-banner',{visible:true,canClose:false});
+        scope.$digest();
+        var closeButton = element.find('.icon_vibrent_x').html();
+        expect(closeButton).to.equal(undefined);
+    });
+
+    it('Should not allow the user to close the modal when canClose is set to true',function () {
+        element = $compile("<vbr-banner name='other-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        BannerService.set('other-banner',{visible:true,canClose:true});
+        scope.$digest();
+        var closeButton = element.find('span')[1];
+        expect(closeButton).to.not.equal(undefined);
+    });
 
 });
