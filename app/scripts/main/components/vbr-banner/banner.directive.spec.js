d300324e2099461688f323b1093f66caacee248a
millsky <kmills@vibrenthealth.com>
2017-09-18 10:07:28 -0400

    Updates to fix issues with test cases faling as a reult of not all files being loaded

--- app/scripts/main/components/vbr-banner/banner.directive.spec.js
+++ app/scripts/main/components/vbr-banner/banner.directive.spec.js
@@ -11,18 +11,41 @@ describe('Banner Directive', function () {
     var $compile,
         $rootScope,
         element,
+        scope,
         $templateCache;
 
     beforeEach(function () {
         angular.mock.module('vbr-style-guide');
     });
 
+    /* Transform HTML into loadable module via ng-html2js*/
     beforeEach(function () {
-        angular.mock.module('/app/scripts/main/components/vbr-banner/banner.html');
+        angular.mock.module('my.templates');
     });
 
+    // Store references to $rootScope and $compile
+    // so they are available to all tests in this describe block
+    beforeEach(inject(function(_$compile_, _$rootScope_,_$templateCache_){
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        scope = $rootScope.$new();
+        $templateCache = _$templateCache_;
+    }));
+
+
     it('Banner directive should load without crashing', function () {
+        element = $compile("<vbr-banner name='test-banner'></vbr-banner>")(scope);
+        scope.$digest();
         expect(true).to.equal(true);
     });
+
+    it('The class should init with success',function () {
+        element = $compile("<vbr-banner name='test-banner'></vbr-banner>")(scope);
+        scope.$digest();
+        var div = element.find('div')[0];
+        expect(div.classList.contains('success')).to.equal(true);
+    });
+
 
 });
