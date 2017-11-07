94f78aa2b1b4a24e6ce181d9f933c4f72095e3f4
millsky <kmills@vibrenthealth.com>
2017-11-07 11:06:07 -0500

    Feature (Buttons)
    Added some basic button styles

--- app/scripts/main/color.controller.js
+++ app/scripts/main/color.controller.js
@@ -7,19 +7,19 @@ import module from './styleguide.module';
 
 
 
-        module.controller('ColorController',
-        ['$http',
+module.controller('ColorController',
+['$http',
 
-            function ColorController($http) {
-                var col = this;
-                $http.get('/variables.json').success(function (data) {
-                    col.colors = {};
-                    for (let name in data.colors) {
-                        let cleanName = name.replace(/\/\/[^\n]+(\n)|\n| |"/g, '');
-                        let cleanColor = data.colors[name].replace(/\n.*/g, '');
-                        col.colors[cleanName] = cleanColor;
-                    }
-                });
-            }]);
+    function ColorController($http) {
+        var col = this;
+        $http.get('/variables.json').success(function (data) {
+            col.colors = {};
+            for (let name in data.colors) {
+                let cleanName = name.replace(/\/\/[^\n]+(\n)|\n| |"/g, '');
+                let cleanColor = data.colors[name].replace(/\n.*/g, '');
+                col.colors[cleanName] = cleanColor;
+            }
+        });
+    }]);
 
 
