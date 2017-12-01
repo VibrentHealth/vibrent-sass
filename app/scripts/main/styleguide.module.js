bcae19f0c70978964f784a0cba84d63dbfdbce3c
Moustafa Baiou <mbaiou@vibrenthealth.com>
2017-12-01 01:22:46 -0500

    modify to import dependent angular modules

--- app/scripts/main/styleguide.module.js
+++ app/scripts/main/styleguide.module.js
@@ -3,15 +3,20 @@
  */
 
 import angular from 'angular';
+import ngAria from 'angular-aria';
+import ngMessages from 'angular-messages';
+import ngTouch from 'angular-touch';
+import uibootstrap from 'angular-ui-bootstrap';
+import ngFileUpload from 'ng-file-upload';
 import styles from '../../styles/main.scss';
 
 let module = angular.module('vbr-style-guide',
     [
-        'ngAria',
-        'ngMessages',
-        'ngTouch',
-        'ui.bootstrap',
-        'ngFileUpload'
+        ngAria,
+        ngMessages,
+        ngTouch,
+        uibootstrap,
+        ngFileUpload
     ]);
 
 export default module;
